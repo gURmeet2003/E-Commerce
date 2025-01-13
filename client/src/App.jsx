1a242c18c712db87d710501a3b1916def0eb852e
Gurmeet <gurmeetrajsnm@gmail.com>
2025-01-13 22:31:38 +0530

    header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./pages/Auth/Login";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
