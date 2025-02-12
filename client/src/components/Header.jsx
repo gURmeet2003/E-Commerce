05dc76ab92169526b1986c7a091e7cef5d752062
Gurmeet <gurmeetrajsnm@gmail.com>
2025-02-12 13:22:07 +0530

    cart is working fine in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { CiSearch } from "react-icons/ci";
 import { LuCircleUser } from "react-icons/lu";
 import { FaShoppingCart } from "react-icons/fa";
@@ -6,50 +6,25 @@ import Logo from "./Logo";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/authcontext";
 import { toast } from "react-toastify";
-//import { useSelector } from "react-redux";
-import { instance } from "../common";
 
 const Header = () => {
-  const [auth, setAuth] = useAuth();
+  const { auth, setAuth, cartquantity, setCartquantity, updateCartQuantity } =
+    useAuth();
   const isLoggedIn = Boolean(auth.token);
-  const [cartquantity, setCartquantity] = useState(0);
-  // const cart = useSelector((state) => state.user.cart);
 
-  // const totalCartQuantity = cart.reduce((acc, item) => acc + item.quantity, 0);
-
-  const handleLogout = (e) => {
+  const handleLogout = () => {
     localStorage.removeItem("auth");
     setAuth({ user: null, token: "" });
     toast.success("Logout Successful");
+    setCartquantity(0);
   };
 
-  const handlecartquantity = async () => {
-    try {
-      const response = await instance.get(
-        `/auth/find-item-by-id/${auth.user._id}`
-      );
-
-      if (response.status === 200) {
-        setCartquantity(response.data.totalquantity);
-      }
-    } catch (e) {
-      if (e.response && e.response.status === 404) {
-        toast.error(e.response.data.message);
-      } else {
-        console.log(e);
-        toast.error(e.response.data.message);
-      }
-    }
-  };
-
-  useEffect(() => {
-    handlecartquantity();
-  }, [auth.user]);
   return (
     <header className="flex items-center justify-between h-14 p-3 bg-gray-400">
       <Link to={"/"}>
         <Logo h={65} w={65} alt="Company Logo" />
       </Link>
+
       <div className="hidden lg:flex items-center rounded-full border border-black">
         <input
           className="h-7 rounded-l-full p-2 text-sm text-gray-700 outline-none placeholder-gray-500"
@@ -66,14 +41,15 @@ const Header = () => {
       <nav className="flex items-center space-x-4 md:space-x-6">
         <button
           aria-label="User Profile"
-          className="flex items-center justify-center h-8 w-8 text-gray-700 "
+          className="flex items-center justify-center h-8 w-8 text-gray-700"
         >
           <LuCircleUser className="h-6 w-6" />
         </button>
+
         <Link to={"/cart"}>
           <button
             aria-label="Shopping Cart"
-            className="flex items-center justify-center h-8 w-8 text-gray-700 "
+            className="flex items-center justify-center h-8 w-8 text-gray-700"
           >
             <span className="relative">
               <FaShoppingCart className="h-6 w-6" />
@@ -83,6 +59,7 @@ const Header = () => {
             </span>
           </button>
         </Link>
+
         {isLoggedIn ? (
           <button
             onClick={handleLogout}
